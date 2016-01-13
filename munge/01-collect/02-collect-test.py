754ddbeed3c03d185d199fa64103f63b6b600178
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-01-13 15:17:36 -0500

    renames

---
+++ munge/01-collect/02-collect-test.py
@@ -0,0 +1,15 @@
+'''
+Collects data (TO BE DETERMINED).
+
+Authors:
+Paul Hendricks
+
+Date:
+2015-12-15
+
+inputs:
+N/A
+
+outputs:
+test.csv.zip
+'''
---
+++ munge/02-prepare/01-train.py
@@ -0,0 +1,41 @@
+'''
+Munges train data.
+
+Authors:
+Paul Hendricks
+
+Date:
+2015-12-15
+
+inputs:
+train.csv.zip
+
+outputs:
+train.h5
+'''
+
+# Load libraries
+import pandas as pd
+import zipfile
+
+# Load data
+original_path = './data/original/'
+file_name = 'train'
+z = zipfile.ZipFile(original_path + file_name + '.csv.zip')
+train = pd.read_csv(z.open(file_name + '.csv'))
+
+# TODO Set column names
+
+# TODO Set column types
+
+# TODO Filter out certain columns
+
+# TODO Filter out certain observations
+
+# TODO Ensure uniqueness at some level
+
+# TODO Define other variables
+
+# TODO Write out data to data/prepped/
+prepped_path = './data/prepped/'
+train.to_hdf(prepped_path + file_name + '.h5', 'table', append=True)
